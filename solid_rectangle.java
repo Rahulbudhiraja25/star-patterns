9c3f6924b9cc6d7cf1cb505f1fe61dae16179780
RahulBudhiraja <91089279+Rahulbudhiraja25@users.noreply.github.com>
2025-06-25 07:31:15 +0530

    Solved rightAngledTriangle problem

--- solid_rectangle.java
+++ solid_rectangle.java
@@ -4,12 +4,12 @@
 // *****
 
 // ---------------------------------------------------------------------------------------------
-public class solid_rectangle{
-    public static void main (String args[]){
-        int a=3,b=5;
+public class solid_rectangle {
+    public static void main(String args[]) {
+        int a = 3, b = 5;
 
-        for(int i=1;i<=a;i++){
-            for(int j=1;j<=b;j++){
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= b; j++) {
                 System.out.print("*");
             }
             System.out.println();
