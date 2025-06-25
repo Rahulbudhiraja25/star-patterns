f23901c1bf1e59afdc66ff815cdc12c1e7bf1e36
RahulBudhiraja <91089279+Rahulbudhiraja25@users.noreply.github.com>
2025-06-25 07:26:12 +0530

    Solved solid Rectangle

--- solid_rectangle.java
+++ solid_rectangle.java
@@ -1,5 +1,18 @@
+// Solid Rectangle
+// *****
+// *****
+// *****
+
+// ---------------------------------------------------------------------------------------------
 public class solid_rectangle{
     public static void main (String args[]){
-        
+        int a=3,b=5;
+
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=b;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
