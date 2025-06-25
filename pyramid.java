41e3044db5f2fbf47114738d4fcbe8052a05d9b3
RahulBudhiraja <91089279+Rahulbudhiraja25@users.noreply.github.com>
2025-06-25 07:55:01 +0530

    Pending Pyramid Problem

---
+++ pyramid.java
@@ -0,0 +1,20 @@
+//    *   
+//   ***  
+//  *****
+// *******
+
+// -----------------------------------------------------------------------
+
+public class pyramid {
+    public static void main(String[] args) {
+        int a=4;
+
+        for(int i=1;i<=a;i++){
+            
+            for(int j=1;j<=a;j++){
+                
+            }
+            
+        }
+    }
+}
